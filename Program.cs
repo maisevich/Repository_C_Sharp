c35c9f7859dcfb4cbbdc3d889a8413112422f09a
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-05 20:53:31 +0300

    Создана первая программа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Форматируем шапку программы
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("********************");
+            Console.WriteLine("**** Мой проект ****");
+            Console.WriteLine("********************");
+            // Основная программа
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine("Hello, World!");
+
+            // Ожидание нажатия клавиши Enter перед завершением работы
+            Console.ReadLine();
+        }
+    }
+}
+
