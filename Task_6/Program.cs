1751d8d08591e1ccf7141e3b661cc7aae5fc1c23
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-06 14:58:32 +0300

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework
+{
+    // Задача 6: Найдите четное число
+
+    class Program
+    {
+        static void Main (string[]args)
+        {
+            Console.WriteLine ("Введите число");
+
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            int temp = a % 2;
+            
+            bool result = temp ==0;
+
+            if(result)
+            {
+            Console.WriteLine ("Четное число");
+            }
+            else
+            {
+                Console.WriteLine("Нечетное число");
+            }
+                     
+        }    
+
+    }
+
+} 
+
