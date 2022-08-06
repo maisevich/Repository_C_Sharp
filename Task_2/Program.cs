4a60d657eb0e80d117b3d6202e66b0386d895293
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-06 15:09:07 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework
+{
+    // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное.
+
+    class Program
+    {
+        static void Main (string[]args)
+        {
+            Console.WriteLine ("Введите число a ");
+
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine ("Введите число b ");
+
+            int b;
+
+            b = int.Parse(Console.ReadLine());
+
+            if(a > b)
+            {
+            Console.WriteLine ("Максимальное число - " + a);
+            }
+            else
+            {
+                Console.WriteLine("Максимальное число - " + b);
+            }
+                     
+        }    
+
+    }
+
+} 
+
+
