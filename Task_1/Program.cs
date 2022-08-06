5d25f468367bb6d816c48409ee0f87d3fd00f1be
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-06 14:18:08 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework
+{
+    // Задача 1: Найдите среднее арифмитическое двух чисел
+
+    class Program
+    {
+        static void Main (string[]args)
+        {
+            double firstValue, secondValue;
+                
+            Console.WriteLine("Введите число 1");
+
+            firstValue = double.Parse(Console.ReadLine());
+        
+            Console.WriteLine ("Введите число 2");
+
+            secondValue = double.Parse(Console.ReadLine());
+
+            double result = (firstValue + secondValue) / 2;
+
+            Console.WriteLine("Среднее арифмитическое двух чисел = " + result);
+        }
+    }
+
+} 
