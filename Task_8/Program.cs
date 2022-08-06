d39581062fd9ac87ffb5f2d4fe311d2218bda8f3
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-06 15:45:11 +0300

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework
+{
+    // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+    class Program
+    {        static void Main (string[]args)
+        {
+            Console.WriteLine ("Введите число n");
+            int n;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0) 
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+      
+    }
+}    
