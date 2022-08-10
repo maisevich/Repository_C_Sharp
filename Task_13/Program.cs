8f76e1f3995d99b4f6a80063fafb1b872bb8d1d5
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-10 22:17:37 +0300

    Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.Write("Введите натуральное число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine("Третья цифра введенного Вами числа : " + anyNumberText[2]);
+}
+else 
+{
+  Console.WriteLine("Третьей цифры в введенном Вами числе нет");
+}
