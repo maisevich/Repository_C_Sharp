6426a8a7f1da528150c885b62275ac4546f0a256
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-10 22:11:49 +0300

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра трехзначного числа:  "+stringNumber[1]);
