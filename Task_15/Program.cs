9dd4e2d3bd13ea2b6ef35c83ae130f90dec73094
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-10 22:22:25 +0300

    Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("Это выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("Такого дня недели не существует");
+  }
+  else Console.WriteLine("Это рабочий день");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
