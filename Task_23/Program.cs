f8ea95ec291be41658996fa9ea166d8f43e79cab
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-13 14:22:03 +0300

    Task23

---
+++ Task_23/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите любое число для того, чтобы программа вовзела в куб все числа от 0 до введенного Вами числа : ");
+int theNumberInTheCube = Convert.ToInt32(Console.ReadLine());
+void Cube(int[] theNumberInTheCube)
+{
+  int counter = 0;
+  int length = theNumberInTheCube.Length;
+  while (counter <  length)
+  {
+    theNumberInTheCube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  while(index < count)
+  {
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+int[] array = new int[theNumberInTheCube+1];
+Cube(array);
+PrintArry(array);
