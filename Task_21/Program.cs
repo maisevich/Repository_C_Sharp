3d24f4d228566ac54fefca08e87e110cc1db9074
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-13 14:21:25 +0300

    Task21

---
+++ Task_21/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello everybody! Task number twenty one!");
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+int Coordinate(string coor, string point)
+{
+    Console.Write($"Введите координату {coor} точки {point}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2)
+{
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+double length =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+Console.WriteLine($"Длина отрезка  {length}");
