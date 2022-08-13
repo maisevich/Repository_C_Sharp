45b742d74f5eb11574315c8f59f2aa0f716849db
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-13 14:30:00 +0300

    Task19v2

---
+++ Task_19version2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] & number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
