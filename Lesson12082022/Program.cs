75aac0fdde825f6cf3776b7f416e5d4c63163871
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-13 14:19:25 +0300

    Lesson12082022

--- Lesson12082022/Program.cs
+++ Lesson12082022/Program.cs
@@ -1,24 +1,5 @@
-﻿Console.WriteLine("Hello World! Homework task number 19");
-{
-    Console.Write("Enter a five-digit  number  ");
-    int newNumber = Convert.ToInt32(Console.ReadLine());
-    {
-        if (newNumber >= 10000 & newNumber <= 99999)
-        {
-            string newNumberText = Convert.ToString(newNumber);
-            if (newNumberText[0] == newNumberText[4] & newNumberText[1] == newNumberText[3])
-            {
-                Console.WriteLine("The number you entered is a palindrome!");
-            }
-            else
-            {
-                Console.WriteLine("The number you entered is NOT a palindrome!");
-            }
-        }
-        else if (newNumber < 10000) 
-        {
-            Console.WriteLine("You entered a number with fewer than five digits! Try again");
-        }
-        else Console.WriteLine("You entered a number with the number of digits greater than five! Try again");
-    }
-}
+﻿Console.Write("What is your name?");
+string name = Convert.ToString(Console.ReadLine());
+var date = DateTime.Now;
+Console.WriteLine("Hello, {0}! Today is {1}, it's {2:HH:mm} now.", name, date.DayOfWeek, date);
+Console.WriteLine($"Hello, {name}! Today is {date.DayOfWeek}, it's {date:HH:mm} now.");
