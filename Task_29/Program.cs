a9f65693c16478bed48953f43d728c2406815f27
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-18 20:34:49 +0300

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int [] numbers = new int[8];
+Console.Write("[");
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 1000);
+    Console.Write(Method (i) + ", ");
+  }
+Console.Write("]");
+int Method (int a)
+{
+    return numbers[a];
+}
