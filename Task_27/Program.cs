4d0722b5f3a7008b41b91b368b133754c6c08536
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-18 20:13:00 +0300

    Task_27_new

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -2,20 +2,12 @@
 
 Console.WriteLine("Task_27");
 Console.Write("Введите число : ");
-int numberN = Convert.ToInt32(Console.ReadLine());
-  int SumNumber(int numberN)
-  {
-    int counter = Convert.ToString(numberN).Length;
-    int advance = 0;
-    int result = 0;
-    for (int i = 0; i < counter; i++)
-    {
-      advance = numberN - numberN % 10;
-      result = result + (numberN - advance);
-      numberN = numberN / 10;
-    }
-   return result;
-  }
-
-int sumNumber = SumNumber(numberN);
-Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("Сумма цифр в числе = " + sum);
