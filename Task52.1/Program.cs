ca0bad5736e8a116ae33c230e44842dea72770a0
Alex Maisevich <maisevich.a.s@gmail.com>
2022-08-27 21:02:15 +0300

    Task52.1

---
+++ Task52.1/Program.cs
@@ -0,0 +1,47 @@
+﻿
+Console.WriteLine();
+
+Console.Write("Задача 52. Задайте двумерный массив из целых чисел.");
+Console.Write("Программа найдет среднее арифметическое элементов в каждом столбце массива");
+
+Console.WriteLine();
+
+Console.WriteLine("Введите количество строк для массива чисел :");
+int stringArray = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Введите количество столбцов для массива чисел :");
+int columnArray = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = new int [stringArray, columnArray];
+double [] sumMiddle = new double [columnArray];
+Random ran = new Random();
+
+for (int i=0; i<stringArray; i++)
+{
+    for(int j=0; j<columnArray; j++)
+    {
+        array[i, j] = ran.Next(0, 100);
+
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+for (int i=0; i<columnArray; i++)
+{
+    for(int j=0; j<stringArray; j++)
+    {
+        sumMiddle[i] += array[j, i];
+    }
+}
+
+Console.WriteLine();
+
+Console.Write("Среднее арифметическое каждого столбца: ");
+for (int i=0; i<columnArray; i++)
+{
+    Console.Write(sumMiddle[i] / stringArray + "; ");
+}
+Console.WriteLine();
