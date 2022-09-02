0c82fdfdacc579421401b2b7a3e0356cb26d8ad6
Alex Maisevich <maisevich.a.s@gmail.com>
2022-09-02 18:42:23 +0300

    Task54

---
+++ Task56/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Task 56.
+*/
+
+Console.WriteLine("Введите размер квадратного массива");
+int massVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[massVol, massVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int minimumSum
+ = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];        
+    }
+    if (sum < minimumSum
+    )
+    {
+        minimumSum
+         = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine
+("Строка с наименьшей суммой - № " + (indexLine));
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
