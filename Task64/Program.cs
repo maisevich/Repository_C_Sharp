b56fac419bf8d8c96f15c78309f2110d9d24111c
Alex Maisevich <maisevich.a.s@gmail.com>
2022-09-03 18:44:39 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1.
+N = 5 -> 5, 4, 3, 2, 1
+*/
+
+Console.WriteLine("Введите любое число больше единицы");
+int lastNumber = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+PrintNumber(lastNumber, count);
+Console.Write(1);
+
+void PrintNumber(int lastNumber, int count)
+{
+  if (count > lastNumber) return;
+  PrintNumber(lastNumber, count + 1);
+  Console.Write(count + ", ");
+}
