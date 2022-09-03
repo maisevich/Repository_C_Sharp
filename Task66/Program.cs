4a5efd76fc50f97501fe4edb56556677513326c2
Alex Maisevich <maisevich.a.s@gmail.com>
2022-09-03 19:34:11 +0300

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите поочережно числа М и N");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+int lastNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сумма натуральных элементов в промежутке от M до N = "); 
+Console.Write(lastNum*(lastNum+1)/2);
